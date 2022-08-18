c064e59d0d1fd33c9c179c79bc2bb1277b0881e9
Storj Dashboard <102604897+storjdashboard@users.noreply.github.com>
2022-08-18 07:36:27 +0100

    Authentication Update
    Added authentication method between the web portal and SNO

--- public/pay.php
+++ public/pay.php
@@ -1,5 +1,16 @@
-<?php 
-header('Content-Type: application/json; charset=utf-8');
+	<?php 
 include("config.php");
+$headers = getallheaders();
+if($headers['Dash-Auth']==$auth){
+header('Content-Type: application/json; charset=utf-8');
 echo file_get_contents("http://$ip/api/sno/estimated-payout"); 
+	}else{
+		if($headers['Dash-Submitted']!=1){
+		header('Content-Type: application/json; charset=utf-8');			
+echo "Unauthorized
+=".$headers['Dash-Auth'];exit;
+		}else{
+		header('HTTP/1.1 401 Unauthorized', true, 401);exit;
+		}
+	}
 ?>
