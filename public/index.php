b8f7100bf1be817b406c09dddb16de235a77dd27
Storj Dashboard <102604897+storjdashboard@users.noreply.github.com>
2022-08-18 07:35:32 +0100

    Authentication Update
    Added authentication method between the web portal and SNO

--- public/index.php
+++ public/index.php
@@ -1,9 +1,19 @@
 <?php 
-header('Content-Type: application/json; charset=utf-8');
 include("config.php");
+$headers = getallheaders();
+if($headers['Dash-Auth']==$auth){
+header('Content-Type: application/json; charset=utf-8');
 $content = file_get_contents("http://$ip/api/sno/"); 
-
 $array = json_decode($content, true);$replacements = array("wallet" => "BLANK", "nodeID" => "BLANK");$array = array_replace($array, $replacements);$array = json_encode($array);
-
 echo $array;
+
+	}else{
+		if($headers['Dash-Submitted']!=1){
+		header('Content-Type: application/json; charset=utf-8');			
+echo "Unauthorized
+=".$headers['Dash-Auth'];exit;
+		}else{
+		header('HTTP/1.1 401 Unauthorized', true, 401);exit;
+		}
+	}
 ?>
