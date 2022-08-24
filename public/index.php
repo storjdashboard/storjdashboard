a2ab862dfb78ee40b6a43de6aef818bd33a4a74c
Storj Dashboard <102604897+storjdashboard@users.noreply.github.com>
2022-08-24 19:17:54 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -13,16 +13,16 @@ if (!function_exists('getallheaders')) {
     }
 }
 $headers = getallheaders();
-if(isset($headers['Dash-Auth'])){
-if($headers['Dash-Auth']==$auth){
+
+if(isset($headers['Dash-Auth']) && $headers['Dash-Auth']==$auth){
 header('Content-Type: application/json; charset=utf-8');
 $content = file_get_contents("http://$ip/api/sno/"); 
 $array = json_decode($content, true);$replacements = array("wallet" => "BLANK", "nodeID" => "BLANK");$array = array_replace($array, $replacements);$array = json_encode($array);
 echo $array;
-}
+
 	}else{
 
-		if(isset($headers['Dash-Submitted']) && $headers['Dash-Submitted']!=1){
+		if($headers['Dash-Submitted']!=1){
 		header('Content-Type: application/json; charset=utf-8');			
 echo "Unauthorized
 =".$headers['Dash-Auth'];exit;
