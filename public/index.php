0f52cdfa3f6a5e2e79341217776c5cd71294c354
Storj Dashboard <102604897+storjdashboard@users.noreply.github.com>
2022-08-24 16:06:33 +0100

    Update index.php
    getallheaders - fix

--- public/index.php
+++ public/index.php
@@ -1,5 +1,16 @@
 <?php 
 include("config.php");
+if (!function_exists('getallheaders')) {
+    function getallheaders() {
+    $headers = [];
+    foreach ($_SERVER as $name => $value) {
+        if (substr($name, 0, 5) == 'HTTP_') {
+            $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+        }
+    }
+    return $headers;
+    }
+}
 $headers = getallheaders();
 if($headers['Dash-Auth']==$auth){
 header('Content-Type: application/json; charset=utf-8');
