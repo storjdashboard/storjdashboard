25dcb42172fe5c2f257b8285970bdbf0452a76e4
Storj Dashboard <102604897+storjdashboard@users.noreply.github.com>
2022-08-26 11:48:58 +0100

    updated 0766 chmod for updating files
    updated 0766 chmod for updating files

--- update/deb_ubuntu/update_git_files.sh
+++ update/deb_ubuntu/update_git_files.sh
@@ -15,16 +15,16 @@ echo ""
 
 #1 index 
 wget -O "/var/www/$hostname_ip/index.php" https://raw.githubusercontent.com/storjdashboard/storjdashboard/main/public/index.php  >> /storjdashboard_logs/git/$log_filename
-chmod 0777 /var/www/$hostname_ip/index.php
+chmod 0766 /var/www/$hostname_ip/index.php
 #2 daily
 wget -O "/var/www/$hostname_ip/daily.php" https://raw.githubusercontent.com/storjdashboard/storjdashboard/main/public/daily.php >> /storjdashboard_logs/git/$log_filename
-chmod 0777 /var/www/$hostname_ip/daily.php
+chmod 0766 /var/www/$hostname_ip/daily.php
 #3 pay
 wget -O "/var/www/$hostname_ip/pay.php" https://raw.githubusercontent.com/storjdashboard/storjdashboard/main/public/pay.php >> /storjdashboard_logs/git/$log_filename
-chmod 0777 /var/www/$hostname_ip/pay.php
+chmod 0766 /var/www/$hostname_ip/pay.php
 #4 audit
 wget -O "/var/www/$hostname_ip/audit.php" https://raw.githubusercontent.com/storjdashboard/storjdashboard/main/public/audit.php >> /storjdashboard_logs/git/$log_filename
-chmod 0777 /var/www/$hostname_ip/audit.php
+chmod 0766 /var/www/$hostname_ip/audit.php
 
 echo '-------------------------------'
 echo '-----  UPDATE COMPLETED  ------'
